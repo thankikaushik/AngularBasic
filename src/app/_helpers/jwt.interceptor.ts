ce0b56dd9a68d428967d20e621c0f919d762f775
kaushik <thanki.kaushik@gmail.com>
2019-05-16 09:01:04 +0530

    latest

---
+++ src/app/_helpers/jwt.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '../_service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    debugger;
+    // add authorization header with jwt token if available
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser && currentUser.token) {
+      req = req.clone({
+        setHeaders:{
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      });
+    }
+    return next.handle(req);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+  JwtInterceptor,
+  ErrorInterceptor,
+  fakeBackendProvider
+} from './_helpers';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
+  declarations: [AppComponent, TodoComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule,],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
